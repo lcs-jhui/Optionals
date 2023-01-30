ae03aaae8069dbd6f994e6721fe4df6735d8d10e
Justin Hui <jhui@lcs.on.ca>
2023-01-30 09:02:54 -0500

    finished page 3

--- Optionals/Exploring Optionals.playground/Pages/Page 4.xcplaygroundpage/Contents.swift
+++ Optionals/Exploring Optionals.playground/Pages/Page 4.xcplaygroundpage/Contents.swift
@@ -15,8 +15,11 @@
  
  */
 // Type the code below this line...
-
-
+var x: String?
+var y: String? = ""
+if x == y {
+    print("variables are e")
+}
 /*:
  
  Is an empty string the same as a *nil* value?
